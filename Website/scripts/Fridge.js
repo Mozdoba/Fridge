f063108c74eadd41ff22e5160ec3b0d6d703a40d
Micheal Ozdoba <mozdoba2@my.bcit.ca>
2018-05-07 23:26:02 -0700

    Added delete and render functionality to all modals

--- Website/scripts/Fridge.js
+++ Website/scripts/Fridge.js
@@ -8,19 +8,47 @@ db.settings(settings);
 //----------------------------------------------------------------------------------------------------------
 //DISPLAY DATA FROM FIRESTORE
 //----------------------------------------------------------------------------------------------------------
-
+// Captures user email and password on login
+var userEmail = "test@test.com";
+var userPass;
 /**
  * @TO-DO:
- * create render function
+ * create render function to add lists to modals in fridgepg3.html - 1 hr
+ * function to grab food items in dairy category - 1 hr
+ * function to grab food items in grains category - 1 hr
+ * function to grab food items in fruits category - 1 hr
+ * function to grab food items in meats category - 1 hr
+ * function to grab food items in vegetables category - 1 hr
+ * render content in dairy modal using Divs - 1 hr
+ * render content in grains modal using Divs - 1 hr
+ * render content in fruits modal using Divs- 1 hr
+ * render content in meats modal using Divs - 1 hr
+ * render content in vegetables modal - 1 hr
+ * connect login page to fridgepg3.html (form attribute action?)
+ * agree on database schema / structure - 30 mins
+ * style unordered list - alphabetically vs timestamp
+ * function to remove food items in dairy category - 1 hr
+ * function to remove food items in grains category - 1 hr
+ * function to remove food items in fruits category - 1 hr
+ * function to remove food items in meats category - 1 hr
+ * function to remove food items in vegetables category - 1 hr
+ * update modal content upon page refresh - 30 min
+ * select button to highlight food items - 1 hr
  * 
  */
 
-// Captures user email and password on login
+
 
 // Shortcuts to DOM Elements.
-const updateListButton = document.querySelectorAll(".display-data");
+const renderGrainsButton = document.querySelector("#render-grains");
+const renderMeatsButton = document.querySelector("#render-meats");
+const renderDairyButton = document.querySelector("#render-dairy");
+const renderFruitsButton = document.querySelector("#render-fruits");
+const renderVegetablesButton = document.querySelector("#render-vegetables");
 const getUsersButton = document.querySelectorAll(".display-users");
 const getFoodButton = document.querySelectorAll(".display-foods");
+const getUserIdButton = document.querySelectorAll(".display-userID");
+const editButton = document.querySelectorAll(".edit-button");
 const wheat_content = document.querySelector('#modal-content-wheat');
 const meats_content = document.querySelector('#modal-content-meats');
 const dairy_content = document.querySelector('#modal-content-dairy');
@@ -28,39 +56,81 @@ const fruits_content = document.querySelector('#modal-content-fruits');
 const vegelists_content = document.querySelector('#modal-content-dairy');
 
 // Events for generating modal content
-updateListButton.forEach((el) => {
-    console.log(el);
-    el.addEventListener("click", getId);
+renderDairyButton.addEventListener("click", function() {
+    var userID = 'LGMsHkEvgKWoxrNqO01y';
+    getSubCollectionDairy(userID);
 });
 
+renderMeatsButton.addEventListener("click", function() {
+    var userID = 'LGMsHkEvgKWoxrNqO01y';
+    getSubCollectionMeats(userID);
+});
+
+var userEmail = "test@test.com";
 getUsersButton.forEach((el) => {
-    console.log(el);
-    el.addEventListener("click", getAllUsersWithEmail);
+    el.addEventListener("click", function() {
+        getAllUsersWithEmail(userEmail);
+    });
 });
 
 getFoodButton.forEach((el) => {
-    console.log(el);
     el.addEventListener("click", getSubCollection);
 });
 
-// Gets User data specified by email
-var userEmail = "test@test.com";
-function getAllUsersWithEmail(userEmail) {
-    console.log("Attempting to retreive Document Data");
-    var query = db.collection("Users").where("email", "==", "test@test.com");
-    query.get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+getUserIdButton.forEach((el) => {
+    el.addEventListener("click", function() {
+        getUserId(userEmail);
+    });
+});
+
+editButton.forEach((el) => {
+    el.addEventListener("click", deleteCheckedBoxes);
+});
 
 
-        });
+// Returns all Documents in specefied query
+function getDocumentsInQuery(query) {
+    query.onSnapshot(function(snapshot) {
+        console.log(snapshot.size);
+        if (!snapshot.size) {
+            console.log("No changes to documents in query!");
+            return;
+        }
+        console.log("Returned changed documents in query");
+        return;
+        /*
+        snapshot.docChanges.forEach(function(change) {
+            if (change.type === 'added') {
+              render(change.doc);
+            }
+          });
+          */
     });
 }
 
-function getId(userEmail) {
+// Renders data on HTML page
+function renderFoodItem(foodDoc, modalBodySelector) {
+    // @TO-DO: make a list and append it to ".modal-content" 
+    renderedDoc = "<input type='checkbox' class='selectable'/><label class='food-item'>&nbsp&nbsp&nbsp&nbsp" + foodDoc.id + "</label>";
+    $(".modal-body-dairy").append(renderedDoc);
+    return renderedDoc; //returns Div
+}
+
+// Deletes Checked checkboxes and all children from DOM
+function deleteCheckedBoxes(modal) {
+    var checkBoxes = document.querySelectorAll(".selectable");
+    for(var i = 0; i < checkBoxes.length; i++) {
+        var checkBox = checkBoxes[i];
+        if (checkBox.checked) {
+            checkBox.parentNode.removeChild(checkBox.nextSibling);
+            checkBox.parentNode.removeChild(checkBox);
+        }
+    }
+}
+
+function getUserId(userEmail) {
     console.log("Attempting to retreive Document ID");
-    var query = db.collection("Users").where("email", "==", "test@test.com");
+    var query = db.collection("Users").where("email", "==", userEmail);
     query.get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             // doc.data() is never undefined for query doc snapshots
@@ -69,35 +139,13 @@ function getId(userEmail) {
         });
     });
 }
-
-// Gets User subcollection Food Item data specified by email
-var useremail = "test@test.com";
-function getSubCollection(getId) {
-    console.log("Attempting to retreive SubCollections");
-    var query = db.collection("Users").doc('LGMsHkEvgKWoxrNqO01y').collection("Food Item");
-    query.get().then(function(querySnapshot) {
-        var list = '<ul class="food-list">';
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            // generate content in modal
-            list += "<li>" + doc.id + "</li>";
-        });
-        list += '</ul>';
-        $(".modal-content").append(list);
-    });
-}
-
-// Renders data on HTML page
-function render(data) {
-// @TO-DO: make a list and append it to ".modal-content" 
-
-}
+var userID = getUserId(userEmail);
+console.log(userID);
 
 // Returns mock data from firestore
-function retrieveData() {
+function retrieveData(userEmail) {
     console.log("Attempting to retreive Document Data");
-    let docRef = db.collection("Food").doc("Dairy");
+    let docRef = db.collection("Users").doc(userID);
     docRef.get().then(function(doc) {
         if (doc.exists) {
             console.log("Document data:", doc.data());
@@ -109,6 +157,52 @@ function retrieveData() {
         console.log("Error getting document:", error);
     });
 }
+
+// Gets User data specified by email
+function getAllUsersWithEmail(userEmail) {
+    console.log("Attempting to retreive User Document");
+    var query = db.collection("Users").where("email", "==", userEmail);
+    query.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+
+
+        });
+    });
+}
+
+
+function getSubCollectionDairy(userID) {
+    console.log("Attempting to retreive 'category: dairy' documents from Food Item subcollection");
+    var query = db.collection("Users").doc(userID)
+    .collection("Food Item").where("category", "==", "dairy");
+    query.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            // generate content in modal
+            renderFoodItem(doc);
+        });
+    });
+}
+
+function getSubCollectionMeats(userID) {
+    console.log("Attempting to retreive 'category: meats' documents from Food Item subcollection");
+    var query = db.collection("Users").doc(userID)
+    .collection("Food Item").where("category", "==", "meats");
+    query.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            // generate content in modal
+            renderFoodItem(doc);
+        });
+    });
+}
+
+
+
 
 var firstname = "Lewis";
 function getAllUsers(firstname) {
@@ -126,50 +220,23 @@ function getAllUsers(firstname) {
     });
 }
 
-
-// Returns all Documents in specefied query
-function getDocumentsInQuery(query) {
-    query.onSnapshot(function(snapshot) {
-        console.log(snapshot);
-        console.log(snapshot.size);
-        if (!snapshot.size) {
-            console.log("No changes to documents in query!");
-            return;
-        } 
-        snapshot.docChanges.forEach(function(change) {
-            if (change.type === 'added') {
-                console.log('Added: ', change.doc.data());
-            }
-            if (change.type === 'modified') {
-                console.log('Modified: ', change.doc.data());
-            }
-            if (change.type === 'removed') {
-                console.log('Removed: ', change.doc.data());
-            }
-            return;
+// Gets User subcollection Food Item data specified by email
+function getSubCollection(getUserId) {
+    console.log("Attempting to retreive SubCollections");
+    var query = db.collection("Users").doc(userID).collection("Food Item");
+    query.get().then(function(querySnapshot) {
+        var list = '<ul class="food-list">';
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            // generate content in modal
+            list += "<li>" + doc.id + "</li>";
         });
+        list += '</ul>';
+        $(".modal-body").append(list);
     });
 }
-
-
-// Returns all Users documents from firestore
-
-
-    /*usersRef.get().then(function(doc) {
-        if(doc.exists) {
-            console.log("'Users' collection data:", doc.data());
-            return doc.data();
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No 'Users' docs!");
-            
-        }
-    }).catch(function(error) {
-        console.log("Error getting 'Users' documents:", error);
-    });
-}*/
 });
-
 
 
 
