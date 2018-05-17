3e04af938c927972cecba7dc49550d8197c165a9
Micheal Ozdoba <mozdoba2@my.bcit.ca>
2018-05-17 15:24:30 -0700

    Added autocomplete functions for input in all forms, styled all inputs in all forms

--- Website/scripts/Fridge.js
+++ Website/scripts/Fridge.js
@@ -584,4 +584,120 @@ function logOut(){
     });
 }
 
+/********************************************
+*                                           *
+*              FORM FUNCTIONS               *
+*                                           *
+********************************************/
+
+
+var fruitsList = ["APPLE", "APPLES", "BANANA", "BANANAS", "BLUEBERRIES", "CANTALOUPE", "CHERRIES", "DATES", "DURIAN", "FIGS", "GRAPES", "MANGO", "NECTARINES", "ORANGE", "ORANGES", "PASSIONFRUIT", "PEACHES", "PEARS", "PINEAPPLE", "RASPBERRIES", "STRAWBERRIES", "WATERMELON"];
+var meatsList = ["BACON", "BEEF", "BOLOGNA", "CATFISH", "CHICKEN", "COD", "CRAB", "HALIBUT", "HAM", "HEN", "LAMB", "LIVER", "LOBSTER", "MUSSELS", "OYSTERS", "PRAWN", "PEPPERONI", "PORK", "RIBS", "SALAMI", "SALMON", "SAUSAGE", "SHRIMP", "SNAPPER", "SOCKEYE", "STEAK", "TILAPIA", "TROUT", "TUNA", "TURKEY", "WEINERS", "VEAL"];
+var vegetablesList = ["ASPARAGUS", "AVOCADO", "BEANS", "BEETS", "BOK", "BOK-CHOY", "BOK CHOY", "BROCCOLI", "BROCCOLINI", "BRUSSEL", "CABBAGE", "CARROT", "CARROTS", "CAULIFLOWER", "CELERY", "CELERY-ROOT", "CHARD", "CHOY", "CILANTRO", "CORN/COB", "CORN", "CUCUMBER", "CUKE", "EGGPLANT", "ENDIVE", "GARLIC", "GINGER", "FENNEL", "JALAPENO", "KALE", "LEEKS", "LETTUCE", "LETTUCE BUTTER", "LETTUCE-BUTTER", "LETTUCE GREEN LEAF", "LETTUCE-GREEN-LEAF", "LETTUCE ICEBURG", "LETTUCE-ICEBURG", "LETTUCE RED LEAF", "LETTUCE-RED-LEAF", "LETTUCE ROMAIN", "LETTUCE-ROMAIN", "MINT", "MUSHROOMS", "ONION", "ONIONS", "PARSLEY", "PARSNIP", "PARSNIPS", "PEAS", "PEPPERS", "PEPPERS GREEN", "PEPPERS-GREEN", "PEPPERS ORANGE", "PEPPERS-ORANGE", "PEPPERS YELLOW", "PEPPERS-YELLOW", "PEPPERS RED", "PEPPERS-RED", "PEPPERS JALAPENO", "POTATO", "POTATOE", "POTATOES", "RADICCHIO", "RADISH", "RADISHES", "RHUBARD", "RUTABAGAS", "RUTABAGA", "SCALLIONS", "SHALLOTS", "SPINACH", "SPROUTS", "SQUASH", "SUI CHOY","SUI-CHOY", "TOMATILLOS", "TOMATO","TOMATOES","YAMS","ZUCHINNI"];
+var dairyList = ["BISCUIT", "BISCUITS", "CHEESE", "CHEESESTRING", "COOKIES", "DIP", "EGGS", "JUICE", "KEFIR", "LEMONADE", "MARGARINE", "MILK", "MOZZARELLA", "PUDDING", "TZATZIKI", "YOGURT"];
+var grainsList = ["BREAD", "BUCKWHEAT", "CEREAL", "CHIA", "CRACKER", "CRACKERS", "LENTIL", "LENTILS", "MILLET", "NOODLE", "NOODLES", "OATMEAL", "PASTA", "QUINOA", "RICE", "TORTILLAS"];
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items override-position");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            b.setAttribute("class", "food-suggestions");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+  }
+
+autocomplete(document.getElementById("myGrainsInput"), grainsList);
+autocomplete(document.getElementById("myMeatsInput"), meatsList);
+autocomplete(document.getElementById("myDairyInput"), dairyList);
+autocomplete(document.getElementById("myFruitsInput"), fruitsList);
+autocomplete(document.getElementById("myVegetablesInput"), vegetablesList);
 });
